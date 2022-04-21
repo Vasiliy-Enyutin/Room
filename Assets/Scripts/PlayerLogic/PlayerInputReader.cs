c423c84292c5444dbcc87340e29ea7b44517eec6
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-04-21 18:24:04 +0300

    [Add] PlayerInputReader

---
+++ Assets/Scripts/PlayerLogic/PlayerInputReader.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace PlayerLogic
+{
+    public class PlayerInputReader : MonoBehaviour
+    {
+        private Vector3 _moveDirection = Vector3.zero;
+
+
+        public Vector3 MoveDirection => _moveDirection;
+
+
+        private void Update()
+        {
+            _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
