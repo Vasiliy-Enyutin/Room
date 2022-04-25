f92ee2b852266b86ce93ce7c9f4b241abdcac207
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-04-25 18:22:41 +0300

    [Add] ClothesButtonEventBroker

---
+++ Assets/Scripts/UI/ClothesButtonEventBroker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PlayerLogic;
+
+namespace UI
+{
+    public static class ClothesButtonEventBroker
+    {
+        public static event Action<ClothesTypes> OnClothesButtonPressed;
+
+        public static void InvokeOnClothesButtonPressed(ClothesTypes clothesType)
+        {
+            OnClothesButtonPressed?.Invoke(clothesType);
+        }
+    }
+}
