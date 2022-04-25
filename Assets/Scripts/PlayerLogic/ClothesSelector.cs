018a49214d4f5843298df1ffab8dc6320fbb8355
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-04-25 17:03:22 +0300

    [Add] ClothesSelector

---
+++ Assets/Scripts/PlayerLogic/ClothesSelector.cs
@@ -0,0 +1,44 @@
+using UI;
+using UnityEngine;
+
+namespace PlayerLogic
+{
+    public class ClothesSelector : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _bodies;
+        [SerializeField] private GameObject[] _pants;
+        [SerializeField] private GameObject[] _boots;
+
+        [SerializeField] private int _currentBodyArrayIndex;
+        [SerializeField] private int _currentPantsArrayIndex;
+        [SerializeField] private int _currentBootsArrayIndex;
+
+
+        private void OnEnable()
+        {
+            ClothesButtonEventBroker.OnClothesButtonPressed += SelectClothesType;
+        }
+
+        private void OnDisable()
+        {
+            ClothesButtonEventBroker.OnClothesButtonPressed -= SelectClothesType;
+        }
+
+        private void SelectClothesType(ClothesTypes clothesType)
+        {
+            if (clothesType == ClothesTypes.Body)
+                ChangeClothes(_bodies, ref _currentBodyArrayIndex);
+            else if (clothesType == ClothesTypes.Pants)
+                ChangeClothes(_pants, ref _currentPantsArrayIndex);
+            else if (clothesType == ClothesTypes.Boots)
+                ChangeClothes(_boots, ref _currentBootsArrayIndex);
+        }
+
+        private void ChangeClothes(GameObject[] clothes, ref int currentIndex)
+        {
+            clothes[currentIndex].SetActive(false);
+            currentIndex = ++currentIndex % clothes.Length;
+            clothes[currentIndex].SetActive(true);
+        }
+    }
+}
